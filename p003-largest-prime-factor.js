a0890fc2ba16027219a385ec5c7c9e7b45be2322
dermio <thisdjay@gmail.com>
2018-12-30 12:01:41 -0800

    Problem 003, Largest Prime Factor

---
+++ p003-largest-prime-factor.js
@@ -0,0 +1,28 @@
+/* Largest prime factor, Problem 3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143? */
+
+function primeFactors(num) {
+  let arr = [];
+  let i = 2;
+
+  if (num <= 1) { return "Enter a number greater than 1"; }
+
+  while (i <= num) {
+    if (num % i === 0) { // Num is divisible & no remainder
+      arr.push(i); // Push i onto array
+      // Num is divisible & no remainder, set num = num / i to reduce num value
+      num /= i;
+      i = 2; // Start i count at 2 again
+    } else {
+      i++;
+    }
+  }
+
+  return arr;
+}
+
+let it = primeFactors(600851475143);
+// [ 71, 839, 1471, 6857 ]
+// Largest prime factor is 6857
